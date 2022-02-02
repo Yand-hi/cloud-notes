0de349c6ea123c3d68ec96bb8127e88d2e9020b6
Yandhi <15877410869@163.com>
2022-02-03 00:24:50 +0800

    restructure the trashDetail with vuex

--- src/store/modules/note.js
+++ src/store/modules/note.js
@@ -60,7 +60,7 @@ const actions = {
   deleteNote({commit}, {noteId}) {
     return Note.deleteNote({noteId})
       .then(res => {
-        commit('deleteNotebook', {noteId})
+        commit('deleteNote', {noteId})
         Message.success(res.msg)
       })
   }
