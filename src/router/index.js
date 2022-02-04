53fdb0a1ebe473c09e0e8f28bcf0d62cc75d85e1
Yandhi <15877410869@163.com>
2022-02-04 17:22:40 +0800

    router Lazy Load

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/Login'
-import Notebooks from '../components/NotebookList'
-import NoteDetail from '../components/NoteDetail'
-import TrashDetail from '../components/TrashDetail'
-import HelloWorld from '../components/HelloWorld'
 
 Vue.use(Router)
 
@@ -19,19 +14,19 @@ export default new Router({
     {
       path: '/',
       alias: '/notebooks',
-      component: Notebooks
+      component: () => import('../components/NotebookList.vue')
     },
     {
       path: '/login',
-      component: Login
+      component: () => import('../components/Login.vue')
     },
     {
       path: '/note',
-      component: NoteDetail
+      component: () => import('../components/NoteDetail.vue')
     },
     {
       path: '/trash',
-      component: TrashDetail
+      component: () => import('../components/TrashDetail.vue')
     }
   ]
 })
